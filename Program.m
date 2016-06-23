3b42bbbf4f7dcbdbf574e661e98ae7a7b612e645
Mohammad Altaleb <mohammadaltaleb@gmail.com>
2016-06-23 17:06:08 +0300

    Update Program.m

--- Program.m
+++ Program.m
@@ -28,40 +28,5 @@ plot(X(:, 2), X * theta, '-');
 legend('Training data', 'Linear regression');
 hold off;
 
-% Visualize The Cost Function
-fprintf('Visualizing J(theta_0, theta_1) ...\n');
-
-% Grid over which we will calculate J
-theta0_vals = linspace(-10, 10, 100);
-theta1_vals = linspace(-1, 4, 100);
-
-% initialize J_vals to a matrix of 0's
-J_vals = zeros(length(theta0_vals), length(theta1_vals));
-
-% Fill out J_vals
-for i = 1:length(theta0_vals)
-    for j = 1:length(theta1_vals)
-	  t = [theta0_vals(i); theta1_vals(j)];    
-	  J_vals(i,j) = ComputeCost(X, y, t);
-    end
-end
-
-% Because of the way meshgrids work in the surf command, we need to 
-% transpose J_vals before calling surf, or else the axes will be flipped
-J_vals = J_vals';
-% Surface plot
-figure;
-surf(theta0_vals, theta1_vals, J_vals)
-xlabel('\theta_0'); ylabel('\theta_1');
-
-% Contour plot
-figure;
-% Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
-xlabel('\theta_0'); ylabel('\theta_1');
-hold on;
-plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
-
-% Predicting Prices
-fprintf('Prediction for 35000:\t%f\n', ([1, 3.5] * theta) * 10000);
-fprintf('Prediction for 70000:\t%f\n', ([1, 7] * theta) * 10000);
+% plotting the cost function
+plot(1: iterations, J_history, '-b');
